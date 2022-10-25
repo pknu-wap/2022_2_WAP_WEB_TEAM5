5dfde2aae3bc761f9603032d7e9bc272faa629f8
unit74 <u__nit@naver.com>
2022-10-26 00:27:13 +0900

    미보안 로그인, 회원가입 구현체 작성

--- server/src/main/java/com/selett/server/repository/UserInfoRepository.java
+++ server/src/main/java/com/selett/server/repository/UserInfoRepository.java
@@ -1,7 +1,11 @@
 package com.selett.server.repository;
 
-import com.selett.server.dto.UserInfoEntity;
+import com.selett.server.mapper.UserInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserInfoRepository extends JpaRepository<UserInfoEntity, Integer> {
+    UserInfoEntity findByIdentificationAndPassword(String identification, String password);
+    Boolean existsByIdentificationAndPassword(String identification, String password);
+    Boolean existsByIdentification(String identification);
+    Boolean existsByEmail(String email);
 }
