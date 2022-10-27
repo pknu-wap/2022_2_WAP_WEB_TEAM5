89e239c03d1b8982420e3c1c6f29bada7f4bb448
unit74 <u__nit@naver.com>
2022-10-28 01:00:34 +0900

    lists table save test

--- server/src/test/java/com/selett/server/list/ListEntityTest.java
+++ server/src/test/java/com/selett/server/list/ListEntityTest.java
@@ -1,7 +1,7 @@
 package com.selett.server.list;
 
+import com.selett.server.mapper.ListEntity;
 import com.selett.server.repository.ListRepository;
-import com.selett.server.repository.UserInfoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,5 +15,16 @@ public class ListEntityTest {
     @Test
     public void searchAll() {
         listRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    public void save() {
+        ListEntity listEntity = new ListEntity();
+        listEntity.setTitle("조금 긴 String 저장해보기");
+        listEntity.setPosition(2);
+        listEntity.setUserId(1);
+
+        listRepository.save(listEntity);
+        searchAll();
     }
 }
