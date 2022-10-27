b5563614453e05712568d93aa00b7e13863ca50e
unit74 <u__nit@naver.com>
2022-10-28 04:19:47 +0900

    user_id에 따른 자기소개서 가져오기

--- server/src/main/java/com/selett/server/main/controller/MainApi.java
+++ server/src/main/java/com/selett/server/main/controller/MainApi.java
@@ -1,20 +1,25 @@
 package com.selett.server.main.controller;
 
 import com.selett.server.main.dto.MainRequest;
+import com.selett.server.main.dto.MainResponse;
+import com.selett.server.main.service.MainService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+import javax.validation.Valid;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("")
 public class MainApi {
-
+    private final MainService mainService;
     @GetMapping("")
-    public void getListAndCoverLetter(MainRequest mainRequest) {
-        log.info(mainRequest.getUserId());
+    public ResponseEntity<MainResponse> getListAndCoverLetter(@Valid MainRequest mainRequest) {
+        MainResponse mainResponse = mainService.getListAndCoverLetter(mainRequest.getUserId());
+
+        return ResponseEntity.ok(mainResponse);
     }
 }
--- server/src/main/java/com/selett/server/main/service/MainService.java
+++ server/src/main/java/com/selett/server/main/service/MainService.java
@@ -1,10 +1,17 @@
 package com.selett.server.main.service;
 
+import com.selett.server.main.dto.CoverLetter;
+import com.selett.server.main.dto.FolderList;
 import com.selett.server.main.dto.MainResponse;
+import com.selett.server.mapper.ListEntity;
 import com.selett.server.repository.CoverLetterRepository;
 import com.selett.server.repository.ListRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +20,31 @@ public class MainService {
     private final CoverLetterRepository coverLetterRepository;
 
     public MainResponse getListAndCoverLetter(Integer userId) {
-        return null;
+        MainResponse response = new MainResponse(new ArrayList<>());
+
+        List<ListEntity> listEntities = listRepository.findAll(Sort.by(Sort.Direction.ASC, "position"));
+        for(ListEntity listEntity : listEntities) {
+            FolderList folderList = new FolderList();
+
+            folderList.setListId(listEntity.getListId());
+            folderList.setTitle(listEntity.getTitle());
+            List<CoverLetter> coverLetters = new ArrayList<>();
+            coverLetterRepository.findAllByListIdOrderByPositionAsc(listEntity.getListId()).forEach(coverLetterEntity -> {
+                CoverLetter coverLetter = new CoverLetter();
+                coverLetter.setId(coverLetterEntity.getId());
+                coverLetter.setTitle(coverLetterEntity.getTitle());
+                coverLetter.setTitleLock(coverLetterEntity.getTitleLock());
+                coverLetter.setDescription(coverLetterEntity.getDescription());
+                coverLetter.setDescriptionLock(coverLetterEntity.getDescriptionLock());
+
+                coverLetters.add(coverLetter);
+            });
+
+            folderList.setCoverLetter(coverLetters);
+
+            response.getList().add(folderList);
+        }
+
+        return response;
     }
 }
