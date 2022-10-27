ffdb24fba63a28eb476023cbfeeb966c1739b198
unit74 <u__nit@naver.com>
2022-10-28 02:47:00 +0900

    유효성 검사 어노테이션 추가

--- server/src/main/java/com/selett/server/login/controller/LoginApi.java
+++ server/src/main/java/com/selett/server/login/controller/LoginApi.java
@@ -4,12 +4,13 @@ import com.selett.server.login.dto.LoginRequest;
 import com.selett.server.login.dto.LoginResponse;
 import com.selett.server.login.service.LoginService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @RestController
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginApi {
     private final LoginService loginService;
     @PostMapping("")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         LoginResponse loginResponse = loginService.login(loginRequest.getIdentification(), loginRequest.getPassword());
 
         return ResponseEntity.ok(loginResponse);
--- server/src/main/java/com/selett/server/register/controller/RegisterApi.java
+++ server/src/main/java/com/selett/server/register/controller/RegisterApi.java
@@ -4,12 +4,13 @@ import com.selett.server.register.dto.RegisterRequest;
 import com.selett.server.register.dto.RegisterResponse;
 import com.selett.server.register.service.RegisterService;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @RestController
@@ -18,7 +19,7 @@ public class RegisterApi {
     private final RegisterService registerService;
 
     @PostMapping("")
-    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
         RegisterResponse registerResponse = registerService.register(registerRequest.getIdentification(), registerRequest.getPassword(), registerRequest.getName(), registerRequest.getEmail());
 
         return ResponseEntity.ok(registerResponse);
