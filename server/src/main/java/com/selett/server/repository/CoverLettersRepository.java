7dc2858a5fde1be4c34e2a608f3227137017ebcf
unit74 <u__nit@naver.com>
2022-10-28 02:29:37 +0900

    클래스명 변경

--- server/src/main/java/com/selett/server/repository/CoverLettersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.selett.server.repository;
-
-import com.selett.server.mapper.CoverLettersEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoverLettersRepository extends JpaRepository<CoverLettersEntity, Integer> {
-}
