c83408253328b5df5226257cbefdff04db5be259
unit74 <u__nit@naver.com>
2022-10-28 04:17:48 +0900

    list_id에 해당하는 cover_letters 찾는 기능 추가

--- server/src/main/java/com/selett/server/repository/CoverLetterRepository.java
+++ server/src/main/java/com/selett/server/repository/CoverLetterRepository.java
@@ -3,5 +3,8 @@ package com.selett.server.repository;
 import com.selett.server.mapper.CoverLetterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CoverLetterRepository extends JpaRepository<CoverLetterEntity, Integer> {
+    List<CoverLetterEntity> findAllByListIdOrderByPositionAsc(Integer listId);
 }
