c8f3e76914e292f9d3670a8d6240dc02cd24a4d1
unit74 <u__nit@naver.com>
2022-10-28 18:43:50 +0900

    Size validation 추가

--- server/src/main/java/com/selett/server/login/dto/LoginRequest.java
+++ server/src/main/java/com/selett/server/login/dto/LoginRequest.java
@@ -5,14 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
     @NotNull
+    @Size(min = 1, max = 20)
     private String identification;
 
     @NotNull
+    @Size(min = 1, max = 20)
     private String password;
 }
