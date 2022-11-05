312f84ca3836f1e5b40ecffd5d98323c3ac67a04
unit74 <u__nit@naver.com>
2022-11-05 20:05:43 +0900

    refactor: JSON 키 값이 snake case를 갖게 수정

--- server/src/main/java/com/selett/server/main/dto/CoverLetter.java
+++ server/src/main/java/com/selett/server/main/dto/CoverLetter.java
@@ -1,5 +1,6 @@
 package com.selett.server.main.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CoverLetter {
     private Integer id;
+
     private String title;
+
     private String question;
+
+    @JsonProperty("question_lock")
     private Boolean questionLock;
+
     private String description;
+
+    @JsonProperty("description_lock")
     private Boolean descriptionLock;
 }
