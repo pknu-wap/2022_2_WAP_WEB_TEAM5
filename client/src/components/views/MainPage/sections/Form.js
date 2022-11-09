59dc4b5a0f8741f444c88d3858deceba5b030052
chaeyeon1 <slide10245@naver.com>
2022-11-09 11:22:38 +0900

    Form과 FormG로 분리되어 있던 컴포넌트를 하나로 합침

--- client/src/components/views/MainPage/sections/Form.js
+++ client/src/components/views/MainPage/sections/Form.js
@@ -7,25 +7,42 @@ import axios from "axios";
 function FormG(props) {
   // 맞춤법 검사가 꺼져있음
   const [Title, setTitle] = useState("");
-  const [Text, setText] = useState("");
+  let [Text, setText] = useState("");
+  const [Grammer, setGrammer] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        "http://ec2-13-209-139-191.ap-northeast-2.compute.amazonaws.com/?userId=1"
-      )
-      .then((response) => {
-        setTitle(response.data.list[0].cover_letter[0].question);
-        setText(response.data.list[0].cover_letter[0].description);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "http://ec2-13-209-139-191.ap-northeast-2.compute.amazonaws.com/?userId=1"
+  //     )
+  //     .then((response) => {
+  //       setTitle(response.data.list[0].cover_letter[0].question);
+  //       setText(response.data.list[0].cover_letter[0].description);
+  //     });
+  // }, []);
 
   const titleHandler = (event) => {
     setTitle(event.currentTarget.value);
+    if (Title.length > 200) {
+      alert("200자 이상입니다.");
+      setTitle(Title.substring(0, 200));
+    }
   };
+
+  useEffect(() => {
+    if (props.grammer === "yes") {
+      setGrammer(true);
+    }
+  }, [props.grammer]);
 
   const textHandler = (event) => {
     setText(event.currentTarget.value);
+
+    // console.log(event.currentTarget.value.length);
+    if (Text.length > 5000) {
+      alert("5000자 이상입니다.");
+      setText(Text.substring(0, 5000));
+    }
   };
 
   const titlebuttonHandler = () => {
@@ -80,142 +97,281 @@ function FormG(props) {
 
   return (
     <Fragment>
-      <div
-        style={{
-          // 흰색 블록
-          backgroundColor: "white",
-          marginTop: "30px",
-          marginLeft: "25px",
-          width: "70%",
-          height: "90%",
-          //   borderRadius: "20px",
-          overflow: "hidden",
-          display: "flex",
-          flexDirection: "column",
-          // justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div // 제목
+      {Grammer ? (
+        <div
           style={{
-            width: "93%",
-            height: "7%",
-            border: "solid 2px #d9d9d9",
-            marginTop: "40px",
-            borderRadius: "5px",
-            display: "flex",
-            // paddingTop: "10px", // 글자가 움직이네
-          }}
-        >
-          <input // 제목 input
-            type="text"
-            style={{
-              marginLeft: "10px",
-              width: "95%",
-              height: "99%",
-              border: "none",
-              outline: "0",
-            }}
-            value={Title}
-            onChange={titleHandler}
-            placeholder="제목을 입력해주세요"
-          ></input>
-
-          <Button // 제목 버튼
-            colorScheme="gray"
-            variant="ghost"
-            style={{
-              width: "40px",
-              height: "90%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "1px",
-              marginLeft: "10px",
-              border: "none",
-              outline: "0",
-            }}
-            onClick={titlebuttonHandler}
-          >
-            <LockIcon />
-          </Button>
-        </div>
-
-        <div // 내용 시작
-          style={{
-            borderRadius: "5px",
-            width: "93%",
-            height: "80%",
-            border: "solid 2px #d9d9d9",
-            marginTop: "20px",
+            // 흰색 블록
+            backgroundColor: "white",
+            marginTop: "30px",
+            marginLeft: "25px",
+            width: "70%",
+            height: "90%",
+            //   borderRadius: "20px",
+            overflow: "hidden",
             display: "flex",
             flexDirection: "column",
+            // justifyContent: "center",
+            alignItems: "center",
+            color: "black",
           }}
         >
-          <div style={{ display: "flex", width: "100%", height: "100%" }}>
-            <textarea // 내용 입력 칸
+          <div // 제목
+            style={{
+              width: "93%",
+              height: "7%",
+              border: "solid 2px #d9d9d9",
+              marginTop: "40px",
+              borderRadius: "5px",
+              display: "flex",
+              // paddingTop: "10px", // 글자가 움직이네
+            }}
+          >
+            <input // 제목 input
               type="text"
               style={{
-                width: "95%",
-                height: "97%",
-                border: "none",
                 marginLeft: "10px",
-                marginTop: "10px",
-                resize: "none",
+                width: "95%",
+                height: "99%",
+                border: "none",
                 outline: "0",
               }}
-              placeholder="내용을 입력해주세요"
-              value={Text}
-              onChange={textHandler}
-            ></textarea>
-            <div
+              value={Title}
+              onChange={titleHandler}
+              placeholder="제목을 입력해주세요"
+              maxLength="200"
+            ></input>
+            <Button // 제목 버튼
+              colorScheme="gray"
+              variant="ghost"
               style={{
+                width: "40px",
+                height: "90%",
                 display: "flex",
-                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: "1px",
+                marginLeft: "10px",
+                border: "none",
+                outline: "0",
               }}
+              onClick={titlebuttonHandler}
             >
-              <Button // 내용 버튼
-                colorScheme="gray"
-                variant="ghost"
+              <LockIcon />
+            </Button>
+          </div>
+          <div // 내용 시작
+            style={{
+              borderRadius: "5px",
+              width: "93%",
+              height: "80%",
+              border: "solid 2px #d9d9d9",
+              marginTop: "20px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ display: "flex", width: "100%", height: "100%" }}>
+              <textarea // 내용 입력 칸
+                type="text"
                 style={{
-                  width: "40px",
-                  height: "40px",
-                  marginTop: "1px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginLeft: "10px",
+                  width: "95%",
+                  height: "97%",
                   border: "none",
+                  marginLeft: "10px",
+                  marginTop: "10px",
+                  resize: "none",
                   outline: "0",
                 }}
-                onClick={textbuttonHandler}
-              >
-                <LockIcon />
-              </Button>
-              <Button // 내용 버튼
-                colorScheme="gray"
-                variant="outline"
+                placeholder="내용을 입력해주세요"
+                value={Text}
+                onChange={textHandler}
+              ></textarea>
+              <div
                 style={{
-                  width: "40px",
-                  height: "40px",
-                  marginTop: "1px",
                   display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginLeft: "10px",
-                  fontSize: "13px",
-                  // border: "none",
-                  // outline: "0",
+                  flexDirection: "column",
                 }}
-                onClick={saveHandler}
               >
-                저장
-              </Button>
+                <Button // 내용 버튼
+                  colorScheme="gray"
+                  variant="ghost"
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    marginTop: "1px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: "10px",
+                    border: "none",
+                    outline: "0",
+                  }}
+                  onClick={textbuttonHandler}
+                >
+                  <LockIcon />
+                </Button>
+                <Button // 내용 버튼
+                  colorScheme="gray"
+                  variant="outline"
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    marginTop: "1px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: "10px",
+                    fontSize: "13px",
+                    // border: "none",
+                    // outline: "0",
+                  }}
+                  onClick={saveHandler}
+                >
+                  저장
+                </Button>
+              </div>
             </div>
+            <Count calc={calc} Text={Text} byteCounter={byteCounter} />
           </div>
-          <Count calc={calc} Text={Text} byteCounter={byteCounter} />
         </div>
-      </div>
+      ) : (
+        <div
+          style={{
+            // 흰색 블록
+            backgroundColor: "white",
+            marginTop: "30px",
+            marginLeft: "25px",
+            width: "95%",
+            height: "90%",
+            //   borderRadius: "20px",
+            overflow: "hidden",
+            display: "flex",
+            flexDirection: "column",
+            // justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div // 제목
+            style={{
+              width: "93%",
+              height: "7%",
+              border: "solid 2px #d9d9d9",
+              marginTop: "40px",
+              borderRadius: "5px",
+              display: "flex",
+              // paddingTop: "10px", // 글자가 움직이네
+            }}
+          >
+            <input // 제목 input
+              type="text"
+              style={{
+                marginLeft: "10px",
+                width: "95%",
+                height: "99%",
+                border: "none",
+                outline: "0",
+              }}
+              value={Title}
+              onChange={titleHandler}
+              placeholder="제목을 입력해주세요"
+            ></input>
+
+            <Button // 제목 버튼
+              colorScheme="gray"
+              variant="ghost"
+              style={{
+                width: "40px",
+                height: "90%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: "1px",
+                marginLeft: "10px",
+                border: "none",
+                outline: "0",
+              }}
+              onClick={titlebuttonHandler}
+            >
+              <LockIcon />
+            </Button>
+          </div>
+
+          <div // 내용 시작
+            style={{
+              borderRadius: "5px",
+              width: "93%",
+              height: "80%",
+              border: "solid 2px #d9d9d9",
+              marginTop: "20px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ display: "flex", width: "100%", height: "100%" }}>
+              <textarea // 내용 입력 칸
+                type="text"
+                style={{
+                  width: "95%",
+                  height: "97%",
+                  border: "none",
+                  marginLeft: "10px",
+                  marginTop: "10px",
+                  resize: "none",
+                  outline: "0",
+                }}
+                placeholder="내용을 입력해주세요"
+                value={Text}
+                onChange={textHandler}
+              ></textarea>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Button // 내용 버튼
+                  colorScheme="gray"
+                  variant="ghost"
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    marginTop: "1px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: "10px",
+                    border: "none",
+                    outline: "0",
+                  }}
+                  onClick={textbuttonHandler}
+                >
+                  <LockIcon />
+                </Button>
+                <Button // 내용 버튼
+                  colorScheme="gray"
+                  variant="outline"
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    marginTop: "1px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: "10px",
+                    fontSize: "13px",
+                    // border: "none",
+                    // outline: "0",
+                  }}
+                  onClick={saveHandler}
+                >
+                  저장
+                </Button>
+              </div>
+            </div>
+            <Count calc={calc} Text={Text} byteCounter={byteCounter} />
+          </div>
+        </div>
+      )}
     </Fragment>
   );
 }
