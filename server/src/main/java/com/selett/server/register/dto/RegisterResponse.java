e468d1ac9cc6a8e66a8677547c85cd67074b8923
unit74 <u__nit@naver.com>
2022-11-11 00:52:16 +0900

    refactor: response 간결화, 응답코드로 변경

--- server/src/main/java/com/selett/server/register/dto/RegisterResponse.java
+++ server/src/main/java/com/selett/server/register/dto/RegisterResponse.java
@@ -8,6 +8,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterResponse {
-    private Boolean success;
-    private String message;
+    private Integer status;
 }
