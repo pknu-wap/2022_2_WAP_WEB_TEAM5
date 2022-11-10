ec2f4495a7197083fa9f070bad472cad1bdbb0a8
unit74 <u__nit@naver.com>
2022-11-11 00:53:36 +0900

    feat: 리스트 추가 req, res

---
+++ server/src/main/java/com/selett/server/main/dto/CreateListRequest.java
@@ -0,0 +1,19 @@
+package com.selett.server.main.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateListRequest {
+    @NotNull
+    @JsonProperty("user_id")
+    private Integer userId;
+
+    private String title;
+}
