f961dd5a927c9ad255f2cb6e0cd7fc36ea06eb48
chaeyeon1 <slide10245@naver.com>
2022-11-16 23:22:08 +0900

    recoil App.js에 적용

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,22 @@ import Account from "./components/views/Account/Account";
 import Profile from "./components/views/Profile/Profile";
 import { ChakraProvider } from "@chakra-ui/react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
+      <RecoilRoot>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Router>
+      </RecoilRoot>
     </ChakraProvider>
   );
 }
