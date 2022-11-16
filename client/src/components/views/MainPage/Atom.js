ee3837a0ce71fbbc15e0208a803349aa8ae7b483
chaeyeon1 <slide10245@naver.com>
2022-11-16 23:23:05 +0900

    recoil의 atom 만들기
    파일 목록, 폴더 클릭했을 때 id, 폴더 목록, 파일 클릭했을 때 id

---
+++ client/src/components/views/MainPage/Atom.js
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+export const CoverState = atom({
+  key: "counter",
+  default: [],
+});
+
+export const folderClickIdState = atom({
+  key: "folderClick",
+  default: 0,
+});
+
+export const CompanyListState = atom({
+  key: "CompanyListSt",
+  default: [],
+});
+
+// export const
+export const fileClickIdState = atom({
+  key: "fileClick",
+  default: 0,
+});
