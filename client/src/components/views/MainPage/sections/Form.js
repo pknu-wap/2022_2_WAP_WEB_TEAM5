799b2c760776a98e340776277a7492654e59f0ae
chaeyeon1 <slide10245@naver.com>
2022-11-17 20:36:54 +0900

    Form 코드 간결화

--- client/src/components/views/MainPage/sections/Form.js
+++ client/src/components/views/MainPage/sections/Form.js
@@ -227,221 +227,110 @@ function Form(props) {
 
   return (
     <Fragment>
-      {Grammer ? (
-        // 맞춤법 검사가 열려있으면?
-        <div
+      <div
+        style={{
+          // 흰색 블록
+          backgroundColor: "white",
+          marginTop: "30px",
+          marginLeft: "25px",
+          width: { Grammer } ? "95%" : "70%",
+          height: "90%",
+          overflow: "hidden",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          color: "black",
+        }}>
+        <div // 제목
           style={{
-            // 흰색 블록
-            backgroundColor: "white",
-            marginTop: "30px",
-            marginLeft: "25px",
-            width: "70%",
-            height: "90%",
-            overflow: "hidden",
+            width: "93%",
+            height: "7%",
+            border: "solid 2px #d9d9d9",
+            marginTop: "40px",
+            borderRadius: "5px",
+            display: "flex",
+          }}>
+          <input // 제목 input
+            type="text"
+            style={{
+              marginLeft: "10px",
+              width: "95%",
+              height: "99%",
+              border: "none",
+              outline: "0",
+            }}
+            value={Title || ""}
+            onChange={titleHandler}
+            placeholder="제목을 입력해주세요"
+            readOnly={questionLock ? true : false}></input>
+          <Button // 제목 버튼
+            colorScheme="gray"
+            variant="ghost"
+            style={{
+              width: "40px",
+              height: "90%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "1px",
+              marginLeft: "10px",
+              border: "none",
+              outline: "0",
+            }}
+            onClick={titlebuttonHandler}>
+            {questionLock ? <LockIcon /> : <UnlockIcon />}
+
+            {/* 제목 버튼 안에 자물쇠 아이콘 */}
+          </Button>
+        </div>
+        <div // 내용 시작
+          style={{
+            borderRadius: "5px",
+            width: "93%",
+            height: "80%",
+            border: "solid 2px #d9d9d9",
+            marginTop: "20px",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
-            color: "black",
           }}>
-          <div // 제목
-            style={{
-              width: "93%",
-              height: "7%",
-              border: "solid 2px #d9d9d9",
-              marginTop: "40px",
-              borderRadius: "5px",
-              display: "flex",
-            }}>
-            <input // 제목 input
+          <div style={{ display: "flex", width: "100%", height: "100%" }}>
+            <textarea // 내용 입력 칸
               type="text"
               style={{
-                marginLeft: "10px",
                 width: "95%",
-                height: "99%",
+                height: "97%",
                 border: "none",
+                marginLeft: "10px",
+                marginTop: "10px",
+                resize: "none",
                 outline: "0",
               }}
-              value={Title || ""}
-              onChange={titleHandler}
-              placeholder="제목을 입력해주세요"
-              readOnly={questionLock ? true : false}></input>
-            <Button // 제목 버튼
+              placeholder="내용을 입력해주세요"
+              value={Text || ""}
+              onChange={textHandler}></textarea>
+            <Button // 내용 버튼
               colorScheme="gray"
               variant="ghost"
               style={{
                 width: "40px",
-                height: "90%",
+                height: "40px",
+                marginTop: "1px",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                marginTop: "1px",
                 marginLeft: "10px",
                 border: "none",
                 outline: "0",
               }}
-              onClick={titlebuttonHandler}>
-              {questionLock ? <LockIcon /> : <UnlockIcon />}
-
-              {/* 제목 버튼 안에 자물쇠 아이콘 */}
+              onClick={textbuttonHandler}>
+              {descriptionLock ? <LockIcon /> : <UnlockIcon />}
+              {/* 내용 버튼안의 자물쇠 아이콘 */}
             </Button>
           </div>
-          <div // 내용 시작
-            style={{
-              borderRadius: "5px",
-              width: "93%",
-              height: "80%",
-              border: "solid 2px #d9d9d9",
-              marginTop: "20px",
-              display: "flex",
-              flexDirection: "column",
-            }}>
-            <div style={{ display: "flex", width: "100%", height: "100%" }}>
-              <textarea // 내용 입력 칸
-                type="text"
-                style={{
-                  width: "95%",
-                  height: "97%",
-                  border: "none",
-                  marginLeft: "10px",
-                  marginTop: "10px",
-                  resize: "none",
-                  outline: "0",
-                }}
-                placeholder="내용을 입력해주세요"
-                value={Text || ""}
-                onChange={textHandler}></textarea>
-              <Button // 내용 버튼
-                colorScheme="gray"
-                variant="ghost"
-                style={{
-                  width: "40px",
-                  height: "40px",
-                  marginTop: "1px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginLeft: "10px",
-                  border: "none",
-                  outline: "0",
-                }}
-                onClick={textbuttonHandler}>
-                {descriptionLock ? <LockIcon /> : <UnlockIcon />}
-                {/* 내용 버튼안의 자물쇠 아이콘 */}
-              </Button>
-            </div>
-            <Count calc={calc} Text={Text} byteCounter={byteCounter} />
-            {/* 글자수를 세주는 폼 */}
-          </div>
+          <Count calc={calc} Text={Text} byteCounter={byteCounter} />
+          {/* 글자수를 세주는 폼 */}
         </div>
-      ) : (
-        <div
-          style={{
-            // 흰색 블록
-            backgroundColor: "white",
-            marginTop: "30px",
-            marginLeft: "25px",
-            width: "95%",
-            height: "90%",
-            overflow: "hidden",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}>
-          <div // 제목
-            style={{
-              width: "93%",
-              height: "7%",
-              border: "solid 2px #d9d9d9",
-              marginTop: "40px",
-              borderRadius: "5px",
-              display: "flex",
-            }}>
-            <input // 제목 input
-              type="text"
-              style={{
-                marginLeft: "10px",
-                width: "95%",
-                height: "99%",
-                border: "none",
-                outline: "0",
-              }}
-              value={Title || ""}
-              onChange={titleHandler}
-              placeholder="제목을 입력해주세요"
-              readOnly={questionLock ? true : false}></input>
-
-            <Button // 제목 버튼
-              colorScheme="gray"
-              variant="ghost"
-              style={{
-                width: "40px",
-                height: "90%",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginTop: "1px",
-                marginLeft: "10px",
-                border: "none",
-                outline: "0",
-              }}
-              onClick={titlebuttonHandler}>
-              {questionLock ? <LockIcon /> : <UnlockIcon />}
-              {/* 자물쇠 */}
-            </Button>
-          </div>
-
-          <div // 내용 시작
-            style={{
-              borderRadius: "5px",
-              width: "93%",
-              height: "80%",
-              border: "solid 2px #d9d9d9",
-              marginTop: "20px",
-              display: "flex",
-              flexDirection: "column",
-            }}>
-            <div style={{ display: "flex", width: "100%", height: "100%" }}>
-              {/* 내용 입력칸과 버튼을 묶는 div */}
-              <textarea // 내용 입력 칸
-                type="text"
-                style={{
-                  width: "95%",
-                  height: "97%",
-                  border: "none",
-                  marginLeft: "10px",
-                  marginTop: "10px",
-                  resize: "none",
-                  outline: "0",
-                }}
-                placeholder="내용을 입력해주세요"
-                value={Text || ""}
-                onChange={textHandler}
-                readOnly={descriptionLock ? true : false}>
-                {/* Lock이 걸려있으면 수정 불가 */}
-              </textarea>
-              <Button // 내용 버튼
-                colorScheme="gray"
-                variant="ghost"
-                style={{
-                  width: "40px",
-                  height: "40px",
-                  marginTop: "1px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginLeft: "10px",
-                  border: "none",
-                  outline: "0",
-                }}
-                onClick={textbuttonHandler}>
-                {descriptionLock ? <LockIcon /> : <UnlockIcon />}
-              </Button>
-            </div>
-            <Count calc={calc} Text={Text} byteCounter={byteCounter} />
-            {/* 글자수 세주는 폼 */}
-          </div>
-        </div>
-      )}
+      </div>
     </Fragment>
   );
 }
