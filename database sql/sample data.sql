40b2609cb6c4764893e9dfd65b84fc5e53ebfed2
unit74 <u__nit@naver.com>
2022-11-20 19:45:02 +0900

    refactor: prev, next 참조키 설정, 임의로 넣을 수 있었던 샘플 삭제

--- database sql/sample data.sql
+++ database sql/sample data.sql
@@ -1,28 +1,6 @@
 INSERT INTO user_informations (identification, password, name, email)
 VALUES ('admin', 'selettadmin', 'admin', 'admin@pukyong.ac.kr');
 
-INSERT INTO lists (title, prev, next, user_id)
-VALUES ('admin test', 0, 2, 1);
-
-INSERT INTO lists (title, prev, next, user_id)
-VALUES ('네이버', 1, 3, 1);
-
-INSERT INTO lists (title, prev, next, user_id)
-VALUES ('카카오', 2, 0, 1);
-
-INSERT INTO cover_letters (title, question, question_lock, description, description_lock, prev, next, list_id)
-VALUES ('1번 문항', '왜 지원했나요', TRUE, '돈 많이주니까', FALSE, 0, 2, 1);
-
-INSERT INTO cover_letters (title, question, question_lock, description, description_lock, prev, next, list_id)
-VALUES ('2번 문항', '와서 뭐할거야', FALSE, '일단 시켜줘', TRUE, 1, 0, 1);
-
-INSERT INTO cover_letters (title, question, question_lock, description, description_lock, prev, next, list_id)
-VALUES ('1번 문항', '네이버 가고 싶어요', TRUE, '이게 내용임', TRUE, 0, 0, 2);
-
-INSERT INTO cover_letters (title, question, question_lock, description, description_lock, prev, next, list_id)
-VALUES ('새 자기소개서', '질문', FALSE, 'KAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKAOAAAAAAAAAAAAAKOAAAAAAAAAAAAKOA', FALSE, 0,
-        0, 3);
-
 INSERT INTO educations (name, major, degree, admission_date, graduation_date, major_grade, major_course, grade,
                         max_grade, course, user_id)
 VALUES ('부경대학교', '컴퓨터공학과 인공지능 소프트웨어 전공', '학사', DATE_FORMAT(now(), '%Y-%m-%d'), DATE_FORMAT(now(), '%Y-%m-%d'), 1.2, 96,
@@ -33,3 +11,6 @@ VALUES ('정보처리기사 1급', DATE_FORMAT(now(), '%Y-%m-%d'), 'test', 1);
 
 INSERT INTO language_skills (title, grade, user_id)
 VALUES ('TOEIC', '990', 1);
+
+INSERT INTO awards (title, date, organization, grade, description, user_id)
+VALUES ('부경대학교 프로그래밍 대회', '2022-01-01', '부경대학교 정보융합대학', '장려상', '장려상임', 1);
