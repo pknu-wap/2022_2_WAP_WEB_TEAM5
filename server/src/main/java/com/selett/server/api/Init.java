5b43e8f33d2f9f4bff2331d783a7b52a1f0b38db
unit74 <u__nit@naver.com>
2022-11-21 20:05:35 +0900

    fix: 모든 유저 정보 삭제(DB 초기화) 기능 추가, 참조키로 인한 삭제 불가능 수정

--- server/src/main/java/com/selett/server/api/Init.java
+++ server/src/main/java/com/selett/server/api/Init.java
@@ -1,19 +1,51 @@
 package com.selett.server.api;
 
-import com.selett.server.jpa.repository.UserInfoRepository;
+import com.selett.server.jpa.repository.*;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/init")
 public class Init {
+    private final AwardRepository awardRepository;
+    private final LanguageSkillRepository languageSkillRepository;
+    private final LicenseRepository licenseRepository;
+    private final MemoRepository memoRepository;
+    private final EducationRepository educationRepository;
+    private final CoverLetterRepository coverLetterRepository;
+    private final ListRepository listRepository;
     private final UserInfoRepository userInfoRepository;
 
     @GetMapping("")
     public void deleteAll() {
-        userInfoRepository.deleteAll();
+        awardRepository.deleteAll();
+        awardRepository.flush();
+        languageSkillRepository.deleteAll();
+        languageSkillRepository.flush();
+        licenseRepository.deleteAll();
+        licenseRepository.flush();
+        memoRepository.deleteAll();
+        memoRepository.flush();
+        educationRepository.deleteAll();
+        educationRepository.flush();
+        coverLetterRepository.findAll().forEach(coverLetterEntity -> {
+            coverLetterEntity.setPrev(null);
+            coverLetterEntity.setNext(null);
+            coverLetterRepository.saveAndFlush(coverLetterEntity);
+        });
+        coverLetterRepository.deleteAll();
+        coverLetterRepository.flush();
+        listRepository.findAll().forEach(listEntity -> {
+            listEntity.setPrev(null);
+            listEntity.setNext(null);
+            listRepository.saveAndFlush(listEntity);
+        });
+        listRepository.deleteAll();
+        listRepository.flush();
     }
 }
