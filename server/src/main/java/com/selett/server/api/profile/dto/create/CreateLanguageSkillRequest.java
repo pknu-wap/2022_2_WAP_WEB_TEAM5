a21962841ddaa0811ba665900a12718e69c4dbe0
unit74 <u__nit@naver.com>
2022-11-22 11:56:18 +0900

    (merge)

---
+++ server/src/main/java/com/selett/server/api/profile/dto/create/CreateLanguageSkillRequest.java
@@ -0,0 +1,22 @@
+package com.selett.server.api.profile.dto.create;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CreateLanguageSkillRequest {
+    @NotNull
+    private String title;
+    @NotNull
+    private String grade;
+    @NotNull
+    @JsonProperty("user_id")
+    private Integer userId;
+}
