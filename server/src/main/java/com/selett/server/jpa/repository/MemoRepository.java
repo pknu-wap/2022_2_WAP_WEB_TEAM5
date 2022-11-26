74b71ac080e9519af3cd48824289ab88c76464ae
unit74 <u__nit@naver.com>
2022-11-26 18:31:54 +0900

    refactor: 사용하지 않는 함수 정리

--- server/src/main/java/com/selett/server/jpa/repository/MemoRepository.java
+++ server/src/main/java/com/selett/server/jpa/repository/MemoRepository.java
@@ -3,8 +3,5 @@ package com.selett.server.jpa.repository;
 import com.selett.server.jpa.mapper.MemoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MemoRepository extends JpaRepository<MemoEntity, Integer> {
-    List<MemoEntity> findAllByUserId(Integer userId);
 }
--- server/src/main/java/com/selett/server/jpa/repository/UserInfoRepository.java
+++ server/src/main/java/com/selett/server/jpa/repository/UserInfoRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 public interface UserInfoRepository extends JpaRepository<UserInfoEntity, Integer> {
     UserInfoEntity findByIdentificationAndPassword(String identification, String password);
-    Boolean existsByIdentificationAndPassword(String identification, String password);
     Boolean existsByIdentification(String identification);
     Boolean existsByEmail(String email);
     Optional<UserInfoEntity> findByIdentification(String identification);
